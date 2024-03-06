4dd7873ad370db5f42394d4bb6e529933bb636d9
YuanXiQWQ <xjr815226031@outlook.com>
2024-03-05 21:59:23 -0800

    demo09

--- Comp1800/demo09/myhike_xing_jiarui/scripts/authentication.js
+++ Comp1800/demo09/myhike_xing_jiarui/scripts/authentication.js
@@ -1,6 +1,5 @@
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
-
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
@@ -41,7 +40,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'main.html',
+  signInSuccessUrl: "main.html",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     //   firebase.auth.GoogleAuthProvider.PROVIDER_ID,
