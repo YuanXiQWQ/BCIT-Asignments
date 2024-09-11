a5579998ef207d685de5b05a1129908ffaf93c00
YuanXi <xjr815226031@outlook.com>
2024-09-10 18:39:00 -0700

    优化Main

--- Comp2522/comp2522 lab 1/src/main/java/ca/bcit/comp2522/bank/Main.java
+++ Comp2522/comp2522 lab 1/src/main/java/ca/bcit/comp2522/bank/Main.java
@@ -1,7 +1,7 @@
 package ca.bcit.comp2522.bank;
 
 /**
- * The main class.
+ * The main class, creates and prints several person and their bank accounts.
  *
  * @author Jiarui Xing
  * @version 1.0
@@ -15,95 +15,90 @@ public class Main {
      */
     public static void main(String[] args) {
         // Albert Einstein
-        Name einsteinName = new Name("Albert", "Einstein");
-        Date einsteinBirth = new Date(1879, 3, 14);
-        Date einsteinDeath = new Date(1955, 4, 18);
-        Date einsteinOpen = new Date(1900, 1, 1);
-        Date einsteinClose = new Date(1950, 10, 14);
-
-        Person einsteinDetails = new Person(einsteinName, einsteinBirth, einsteinDeath);
-        BankClient einstein =
-                new BankClient(einsteinName, einsteinBirth, einsteinDeath, "123456",
-                        einsteinOpen);
-        BankAccount einsteinAccount =
-                new BankAccount(einstein, "abc123", einsteinOpen, einsteinClose, 3141);
-
-        einsteinAccount.deposit(1000);
-        einsteinAccount.withdraw(100, 3141);
-        einsteinAccount.closeAccount(new Date(1950, 10, 14));
-
-        System.out.println(einstein.getName().getInitials());
-        System.out.println(einstein.getName().getFullName());
-        System.out.println(einstein.getName().getReverseName());
-        System.out.println(einsteinDetails.getDetails());
-        System.out.println(einsteinAccount.getDetails());
+        createAndPrintAccount(
+                new Name("Albert", "Einstein"),
+                new Date(1879, 3, 14),
+                new Date(1955, 4, 18),
+                "abc123",
+                new Date(1900, 1, 1),
+                new Date(1950, 10, 14),
+                3141,
+                1000,
+                100
+        );
 
         // Nelson Mandela
-        Name mandelaName = new Name("Nelson", "Mandela");
-        Date mandelaBirth = new Date(1918, 7, 18);
-        Date mandelaDeath = new Date(2013, 12, 5);
-        Date mandelaOpen = new Date(1994, 5, 10);
-        Date mandelaClose = null;
-
-        Person mandelaDetails = new Person(mandelaName, mandelaBirth, mandelaDeath);
-        BankClient mandela =
-                new BankClient(mandelaName, mandelaBirth, mandelaDeath, "654321",
-                        mandelaOpen);
-        BankAccount mandelaAccount =
-                new BankAccount(mandela, "654321", mandelaOpen, mandelaClose, 4664);
-
-        mandelaAccount.deposit(2000);
-        mandelaAccount.withdraw(200, 4664);
-
-        System.out.println(mandela.getName().getInitials());
-        System.out.println(mandela.getName().getFullName());
-        System.out.println(mandela.getName().getReverseName());
-        System.out.println(mandelaDetails.getDetails());
-        System.out.println(mandelaAccount.getDetails());
+        createAndPrintAccount(
+                new Name("Nelson", "Mandela"),
+                new Date(1918, 7, 18),
+                new Date(2013, 12, 5),
+                "654321",
+                new Date(1994, 5, 10),
+                null,
+                4664,
+                2000,
+                200
+        );
 
         // Frida Kahlo
-        Name kahloName = new Name("Frida", "Kahlo");
-        Date kahloBirth = new Date(1907, 7, 6);
-        Date kahloDeath = new Date(1954, 7, 13);
-        Date kahloOpen = new Date(1940, 1, 1);
-        Date kahloClose = new Date(1954, 7, 13);
-
-        Person kahloDetails = new Person(kahloName, kahloBirth, kahloDeath);
-        BankClient kahlo =
-                new BankClient(kahloName, kahloBirth, kahloDeath, "frd123", kahloOpen);
-        BankAccount kahloAccount =
-                new BankAccount(kahlo, "frd123", kahloOpen, kahloClose, 1907);
-
-        kahloAccount.deposit(500);
-        kahloAccount.withdraw(50, 1907);
-        kahloAccount.closeAccount(new Date(1954, 7, 13));
-
-        System.out.println(kahlo.getName().getInitials());
-        System.out.println(kahlo.getName().getFullName());
-        System.out.println(kahlo.getName().getReverseName());
-        System.out.println(kahloDetails.getDetails());
-        System.out.println(kahloAccount.getDetails());
+        createAndPrintAccount(
+                new Name("Frida", "Kahlo"),
+                new Date(1907, 7, 6),
+                new Date(1954, 7, 13),
+                "frd123",
+                new Date(1940, 1, 1),
+                new Date(1954, 7, 13),
+                1907,
+                500,
+                50
+        );
 
         // Jackie Chan
-        Name chanName = new Name("Jackie", "Chan");
-        Date chanBirth = new Date(1954, 4, 7);
-        Date chanDeath = null;
-        Date chanOpen = new Date(1980, 10, 1);
-        Date chanClose = null;
+        createAndPrintAccount(
+                new Name("Jackie", "Chan"),
+                new Date(1954, 4, 7),
+                null,
+                "chan789",
+                new Date(1980, 10, 1),
+                null,
+                1954,
+                3000,
+                500
+        );
+    }
 
-        Person chanDetails = new Person(chanName, chanBirth, chanDeath);
-        BankClient chan =
-                new BankClient(chanName, chanBirth, chanDeath, "chan789", chanOpen);
-        BankAccount chanAccount =
-                new BankAccount(chan, "chan789", chanOpen, chanClose, 1954);
+    /**
+     * Helper method to create a person, bank client, and bank account and print their details.
+     *
+     * @param name      the person's name
+     * @param birthDate the person's birthdate
+     * @param deathDate the person's death date (can be null if alive)
+     * @param accountId the bank account ID
+     * @param openDate  the date the account was opened
+     * @param closeDate the date the account was closed (can be null if still open)
+     * @param pin       the account's PIN
+     * @param deposit   the initial deposit amount
+     * @param withdraw  the amount to withdraw
+     */
+    private static void createAndPrintAccount(Name name, Date birthDate, Date deathDate,
+                                              String accountId, Date openDate, Date closeDate,
+                                              int pin, double deposit, double withdraw) {
+        Person personDetails = new Person(name, birthDate, deathDate);
+        BankClient client = new BankClient(name, birthDate, deathDate, accountId, openDate);
+        BankAccount account = new BankAccount(client, accountId, openDate, closeDate, pin);
 
-        chanAccount.deposit(3000);
-        chanAccount.withdraw(500, 1954);
+        account.deposit(deposit);
+        account.withdraw(withdraw, pin);
 
-        System.out.println(chan.getName().getInitials());
-        System.out.println(chan.getName().getFullName());
-        System.out.println(chan.getName().getReverseName());
-        System.out.println(chanDetails.getDetails());
-        System.out.println(chanAccount.getDetails());
+        if (closeDate != null) {
+            account.closeAccount(closeDate);
+        }
+
+        System.out.println(name.getInitials());
+        System.out.println(name.getFullName());
+        System.out.println(name.getReverseName());
+        System.out.println(personDetails.getDetails());
+        System.out.println(account.getDetails());
+        System.out.println("------------------------");
     }
 }
