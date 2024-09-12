a7f01757574669cb629d8472bf2ae1ced3eab3c8
YuanXi <xjr815226031@outlook.com>
2024-09-12 12:16:43 -0700

    更改项目结构,完成q2

---
+++ Comp2510/Lab2/q1/factorial.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * Get factorial of a number, will check if the number is valid.
+ *
+ * @param numberToFactor the number to get factorial
+ * @return the factorial of the number
+ */
+long factorial(const int numberToFactor) {
+    // Check if the number is valid.
+    if (numberToFactor < 0 || numberToFactor > 17) {
+        // Factorial of negative number is undefined in math, and I tested long will overflow when number is bigger than 17.
+        return -1;
+    } else if (numberToFactor == 0) {
+        // 0! is defined as 1.
+        return 1;
+    }
+
+    // factorial end with 1, using long to avoid overflow because factorial result always big.
+    long result = 1;
+    for (int i = 1; i <= numberToFactor; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    int startNumber = 1;
+    int endNumber = 10;
+    // Form title.
+    printf("| NUMBER | FACTORIAL |\n");
+
+    // From start number to end number.
+    for (int i = startNumber; i < endNumber; i++) {
+        long result = factorial(i);
+        if (result != -1) {
+            // Number is valid.
+            printf("| %6d | %9ld |\n", i, result);
+        } else {
+            // Number is invalid, check why it is invalid.
+            char *issue = i > 17 ? "Overflow" : "N/A";
+            printf("| %6d | %9s |\n", i, issue);
+        }
+    }
+    return 0;
+}
