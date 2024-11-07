0f821b8c268e899c544fa6edab807c601eb019a3
YuanXi <xjr815226031@outlook.com>
2024-11-07 10:41:05 -0800

    init Lab7

---
+++ CST/Term2/Comp2510/Lab7/student.h
@@ -0,0 +1,12 @@
+#ifndef LAB7_STUDENT_H
+#define LAB7_STUDENT_H
+
+#endif //LAB7_STUDENT_H
+
+typedef struct Student {
+    char firstName[64];
+    char lastName[64];
+    char studentID[64];
+    int midterm;
+    int final;
+} Student;
