0d788968f534c3d2f1cbf0b59e0344ea761f1a66
YuanXi <xjr815226031@outlook.com>
2025-09-26 12:06:36 -0700

    COMP3717Lab4 可选实现

--- CST/Term3/Comp3717/Labs/Lab4JerryXing/app/src/main/java/com/bcit/lab4jerryxing/Main.kt
+++ CST/Term3/Comp3717/Labs/Lab4JerryXing/app/src/main/java/com/bcit/lab4jerryxing/Main.kt
@@ -1,21 +1,71 @@
-package com.bcit.lab4jerryxing;
+package com.bcit.lab4jerryxing
 
 /*
    Jerry Xing
    A01354731
 */
 
+/**
+ * Returns "An" if the race starts with a vowel, otherwise returns "A"
+ */
+private fun articleFor(race: String): String =
+    if ("AEIOUaeiou".contains(race.first())) "An" else "A"
+
+/**
+ * Reads a choice from the user
+ */
+private fun readChoice(prompt: String, options: Map<String, String>): String {
+    while (true) {
+        println(prompt)
+        options.forEach { (k, v) -> println("[$k] $v") }
+        print("> ")
+        when (val s = readLine()?.trim()) {
+            in options.keys -> return options[s]!!
+            else -> println("Invalid choice, try again.\n")
+        }
+    }
+}
+
+/**
+ * Reads an integer from the user
+ */
+private fun readInt(prompt: String, min: Int = 1, max: Int = 10): Int {
+    while (true) {
+        print("$prompt ($min..$max)\n> ")
+        val n = readLine()?.trim()?.toIntOrNull()
+        if (n != null && n in min..max) return n
+        println("Invalid number, try again.\n")
+    }
+}
+
+/**
+ * Main
+ */
 fun main() {
     val manager = MissionManager()
 
-    // Dwarf
-    val dwarf = Dwarf()
-    val gather = Gather(dwarf)
+    // Choose minion
+    val race = readChoice(
+        prompt = "Choose a minion race:",
+        options = mapOf("1" to "Dwarf", "2" to "Elf")
+    )
+    val minion: Minion = if (race == "Dwarf") Dwarf() else Elf()
 
-    manager.select(gather, object : MissionListener {
+    // Choose mission
+    val missionType = readChoice(
+        prompt = "Choose a mission:",
+        options = mapOf("1" to "Gather", "2" to "Hunt")
+    )
+
+    // Common anonymous MissionListener
+    val listener = object : MissionListener {
         override fun missionStart(minion: Minion) {
-            val article = if ("AEIOUaeiou".contains(minion.race.first())) "An" else "A"
-            println("$article ${minion.race} was sent off to gather some resources")
+            val a = articleFor(minion.race)
+            if (missionType == "Gather") {
+                println("$a ${minion.race} was sent off to gather some resources")
+            } else {
+                println("$a ${minion.race} started a hunt!")
+            }
         }
 
         override fun missionProgress() {
@@ -23,38 +73,30 @@ fun main() {
         }
 
         override fun missionComplete(minion: Minion, reward: String) {
-            val article = if ("AEIOUaeiou".contains(minion.race.first())) "An" else "A"
-            if (reward == "nothing") {
-                println("$article ${minion.race} has returned from a gather but found nothing!")
+            val a = articleFor(minion.race)
+            if (missionType == "Gather") {
+                if (reward == "nothing") {
+                    println("$a ${minion.race} has returned from a gather but found nothing!")
+                } else {
+                    println("$a ${minion.race} has returned from a gather and found $reward!")
+                }
             } else {
-                println("$article ${minion.race} has returned from a gather and found $reward!")
+                if (reward == "nothing") {
+                    println("$a ${minion.race} has returned from a hunt, but found nothing!")
+                } else {
+                    println("$a ${minion.race} has returned from a hunt, and found $reward!")
+                }
             }
         }
-    })
+    }
 
-    println()
-
-    // Elf
-    val elf = Elf()
-    val hunt = Hunt(elf)
-
-    manager.select(hunt, object : MissionListener {
-        override fun missionStart(minion: Minion) {
-            val article = if ("AEIOUaeiou".contains(minion.race.first())) "An" else "A"
-            println("$article ${minion.race} started a hunt!")
-        }
-
-        override fun missionProgress() {
-            println("...")
-        }
-
-        override fun missionComplete(minion: Minion, reward: String) {
-            val article = if ("AEIOUaeiou".contains(minion.race.first())) "An" else "A"
-            if (reward == "nothing") {
-                println("$article ${minion.race} has returned from a hunt, but found nothing!")
-            } else {
-                println("$article ${minion.race} has returned from a hunt, and found $reward!")
-            }
-        }
-    })
+    // Create and run the mission
+    if (missionType == "Gather") {
+        val times = readInt("How many times to repeat gather?", min = 1)
+        val gather = Gather(minion)
+        manager.selectRepeatable(gather, times, listener)
+    } else {
+        val hunt = Hunt(minion)
+        manager.select(hunt, listener)
+    }
 }
