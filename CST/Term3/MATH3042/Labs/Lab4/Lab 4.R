aeafe35984e047dc52c10ea20985f8a1b79390c4
YuanXi <xjr815226031@outlook.com>
2025-09-26 14:24:27 -0700

    MATH3042Lab4

--- CST/Term3/MATH3042/Labs/Lab4/Lab 4.R
+++ CST/Term3/MATH3042/Labs/Lab4/Lab 4.R
@@ -1,3 +1,4 @@
+#---
 sample(c(1, 2, 3, 4, 5, 6),
        5,
        replace=TRUE,
@@ -9,7 +10,7 @@ sample(c("H", "T"), 1)
 sample(c("H", "T"), 10, replace = TRUE)
 
 
-
+#---
 FlipOnce <-function(){
   result <- sample(c("H ", "T"), 1)
   return(result)
@@ -30,7 +31,7 @@ ProbHeads <- function(m.trials){
 }
 
 ProbHeads(100)
-
+sample(c("H", "T"), 10, replace=TRUE)
 
 #Q2
 FlipCoins <- function(n){
@@ -38,6 +39,7 @@ FlipCoins <- function(n){
 }
 FlipCoins(100)
 
+#---
 replicate(5,FlipCoins(10))
 
 #Q3
@@ -62,3 +64,88 @@ MinAndMaxProbHeads(10000, 10)
 MinAndMaxProbHeads(10000, 100)
 MinAndMaxProbHeads(10000, 1000)
 MinAndMaxProbHeads(10000, 10000)
+
+#---
+die.list <- c(3,6,3,1,2,6,4,2,1,5)
+die.table <- table(die.list)
+die.table
+barplot(die.table)
+
+#Q4
+RollDice <- function(m.trials){
+  rolls <- sample(1:6, m.trials, replace = TRUE)
+  roll.table <- table(rolls)
+  barplot(roll.table,
+          main = paste("Outcome of", m.trials, "Die Rolls"),
+          xlab = "Die Face",
+          ylab = "Frequency")
+}
+
+RollDice(100)
+RollDice(1000)
+RollDice(10000)
+
+#---
+sample(1:6, 5, replace=TRUE)
+
+#Q5
+RollSomeDice <- function(m.trials, n.dice){
+  X <- numeric(m.trials)
+  
+  for(i in 1:m.trials){
+    rolls <- sample(1:6, n.dice, replace=TRUE)
+    X[i] <- sum(rolls == 3)
+  }
+  
+  X.table <- table(X)
+  
+  barplot(X.table,
+          main = paste("Distribution of X (number of 3s out of", 
+                       n.dice, 
+                       "dice) Based on", 
+                       m.trials, 
+                       "Trials"),
+          xlab = "Number of 3s (X)",
+          ylab = "Frequency")
+  
+  return(X.table)
+}
+
+set.seed(69)
+RollSomeDice(10000, 1)
+RollSomeDice(10000, 2)
+RollSomeDice(10000, 5)
+RollSomeDice(10000, 10)
+RollSomeDice(10000, 100)
+
+#Q6
+DrawCards <- function(m.trials, n.cards, replace){
+  deck <- c(rep("R", 26), rep("B", 26))
+  
+  results <- replicate(
+    m.trials,
+    sum(sample(deck, n.cards, replace = replace) == "R")
+  )
+  
+  hist(
+    results,
+    breaks = seq(-0.5, n.cards + 0.5, 1),
+    main = paste("Distribution of Red Cards (n =", 
+                 n.cards, 
+                 ", m =", 
+                 m.trials, 
+                 ", replace =", 
+                 replace, ")"),
+    xlab = "Number of Red Cards")
+}
+
+DrawCards(10000, 1, TRUE)
+DrawCards(10000, 5, TRUE)
+DrawCards(10000, 10, TRUE)
+DrawCards(10000, 20, TRUE)
+DrawCards(10000, 50, TRUE)
+DrawCards(10000, 1, FALSE)
+DrawCards(10000, 5, FALSE)
+DrawCards(10000, 10, FALSE)
+DrawCards(10000, 20, FALSE)
+DrawCards(10000, 50, FALSE)
