439fb956018f8aed100702804b6126ac5e2d5f0f
YuanXi <xjr815226031@outlook.com>
2025-11-03 09:27:32 -0800

    Q2 init

--- CST/Term3/Comp3940/Assignment4/AssignmentIV/src/ThreadRace.java
+++ CST/Term3/Comp3940/Assignment4/AssignmentIV/src/ThreadRace.java
@@ -2,11 +2,25 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class ThreadRace {
+
     public static void main(String[] args)
     {
+
         RaceStatus raceStatus = new RaceStatus(500);
-        new SimpleThread(raceStatus).start();
-        new SimpleThread(raceStatus).start();
+
+        for(int i = 0; i < 3; i++)
+        {
+            new SimpleThread(raceStatus).start();
+        }
+
+        raceStatus.block(1);
+        try
+        {
+            Thread.sleep((long) (4000));
+        } catch(InterruptedException e)
+        {
+        }
+        raceStatus.unblock();
         while(true)
         {
             raceStatus.showRace();
@@ -20,39 +34,9 @@ public class ThreadRace {
     }
 }
 
-class SimpleThread extends Thread {
-    private int curDistance = 0;
-
-    public SimpleThread(RaceStatus raceStatus)
-    {
-        raceStatus.addMe(this);
-    }
-
-    public int getCount()
-    {
-        return curDistance;
-    }
-
-    @Override
-    public void run()
-    {
-        for(int i = 0; i < 10000; i++)
-        {
-            try
-            {
-                sleep((long) (Math.random() * 100));
-            } catch(InterruptedException e)
-            {
-            }
-            curDistance++;
-        }
-    }
-}
-
 class RaceStatus {
     private ArrayList<Thread> threads = null;
     private int maxDistance = 0;
-
 
     public RaceStatus(int maxDistance)
     {
@@ -60,12 +44,10 @@ class RaceStatus {
         this.maxDistance = maxDistance;
     }
 
-
     public void addMe(Thread thread)
     {
         threads.add(thread);
     }
-
 
     public void showRace()
     {
@@ -84,6 +66,74 @@ class RaceStatus {
         {
             System.out.println("Race Over !!!");
             System.exit(0);
+        }
+    }
+
+    public void block(int loserThread)
+    {
+        if(loserThread >= 0 && loserThread < threads.size())
+        {
+            ((SimpleThread) threads.get(loserThread)).makeItWait();
+        }
+    }
+
+    public void unblock()
+    {
+        synchronized(this)
+        {
+            try
+            {
+                this.notifyAll();
+            } catch(Exception e)
+            {
+            }
+        }
+    }
+}
+
+class SimpleThread extends Thread {
+    private RaceStatus raceStatus = null;
+    private int curDistance = 0;
+    private boolean shouldWait = false;
+
+    public SimpleThread(RaceStatus raceStatus)
+    {
+        this.raceStatus = raceStatus;
+        raceStatus.addMe(this);
+    }
+
+    public int getCount()
+    {
+        return curDistance;
+    }
+
+    public void makeItWait() {shouldWait = true;}
+
+    @Override
+    public void run()
+    {
+        for(int i = 0; i < 10000; i++)
+        {
+            try
+            {
+                sleep((long) (Math.random() * 100));
+            } catch(InterruptedException e)
+            {
+            }
+            curDistance++;
+            if(shouldWait)
+            {
+                shouldWait = false;
+                synchronized(this)
+                {
+                    try
+                    {
+                        wait();
+                    } catch(Exception e)
+                    {
+                    }
+                }
+            }
         }
     }
 }
