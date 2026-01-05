1c89b66607b2d2a09e51f2ef3db628cdb8c58805
YuanXi <xjr815226031@outlook.com>
2026-01-05 15:11:52 -0800

    Init Term4
    好累TAT

---
+++ CST/Term4/COMP4945/Lecs/Lec1/OrgMgmt/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace OrgMgmt.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+    }
+}
